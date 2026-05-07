51993f512a442b3fc2625032fa3a1d6e819f2db7
younglin90 <younglin90@gmail.com>
2026-05-07 23:13:01 +0900

    experiment: extend WildMesh underbudget rebudget passes

--- core/generator/tier_wildmesh.py
+++ core/generator/tier_wildmesh.py
@@ -679,7 +679,7 @@ class TierWildMeshGenerator:
             )
             _passes = max(
                 0,
-                int(os.environ.get("AUTO_TESSELL_WILDMESH_CELL_REBUDGET_PASSES", "6")),
+                int(os.environ.get("AUTO_TESSELL_WILDMESH_CELL_REBUDGET_PASSES", "8")),
             )
             for _rb_pass in range(_passes):
                 _n_tets = int(tet_f.shape[0])
